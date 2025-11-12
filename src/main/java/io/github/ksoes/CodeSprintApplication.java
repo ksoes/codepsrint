99955c8962b59c7340d5fea01db8817f47e49b84
kyoungsin <sys1769@nis.co.kr>
2025-11-12 15:42:16 +0900

    프로젝트 생성

---
+++ src/main/java/io/github/ksoes/CodeSprintApplication.java
@@ -0,0 +1,8 @@
+package io.github.ksoes;
+
+public class CodeSprintApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("깃 연동");
+    }
+}
